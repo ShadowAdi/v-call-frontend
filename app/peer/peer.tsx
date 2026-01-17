3c95493368234c4715d509aafb5d8089c021cadc
ShadowAdi <shadowshukla76@gmail.com>
2026-01-17 16:38:08 +0530

    added assign id logic

--- app/peer/peer.tsx
+++ app/peer/peer.tsx
@@ -1,15 +1,17 @@
 // src/app/peer/peer.tsx
 "use client"
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
  
 const PeerPage = () => {
   const myVideoRef = useRef<HTMLVideoElement>(null);
 
+  const [myUniqueId, setMyUniqueId] = useState<string>("");
+
+  const generateRandomString = () => Math.random().toString(36).substring(2);
+
   useEffect(() => {
         if (typeof window !== 'undefined') { 
-          //  Here, we retrieve the camera from the browser 
-          //  and assign the video stream to the reference of our video tag.
           navigator.mediaDevices.getUserMedia({
             video: true,
             audio: true,
@@ -21,10 +23,18 @@ const PeerPage = () => {
         }
   }, []);
 
+  //  Here, when the component is mounted for the first time, 
+  //  we assign a random ID to our state myId.
+  useEffect(() => {
+    setMyUniqueId(generateRandomString);
+  }, [])
+
   return (
     <div className='flex flex-col justify-center items-center p-12'>
-        <video className='w-72' playsInline ref={myVideoRef} autoPlay />
+      <p>your id : {myUniqueId}</p>
+      <video className='w-72' playsInline ref={myVideoRef} autoPlay />
     </div>
   );
 };
-export default PeerPage
+
+export default PeerPage;
