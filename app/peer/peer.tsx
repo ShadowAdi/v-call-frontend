e4ba6248e740cfebb5514986486e5e7c9c4f8430
ShadowAdi <shadowshukla76@gmail.com>
2026-01-17 16:36:30 +0530

    improve video calling

--- app/peer/peer.tsx
+++ app/peer/peer.tsx
@@ -1,9 +1,30 @@
+// src/app/peer/peer.tsx
+"use client"
+
+import { useEffect, useRef } from 'react';
+ 
 const PeerPage = () => {
+  const myVideoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+        if (typeof window !== 'undefined') { 
+          //  Here, we retrieve the camera from the browser 
+          //  and assign the video stream to the reference of our video tag.
+          navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true,
+          }).then(stream => {
+            if (myVideoRef.current) {
+              myVideoRef.current.srcObject = stream;
+            }
+          });
+        }
+  }, []);
+
   return (
-    <div>
-        <p>Hello World!</p>
+    <div className='flex flex-col justify-center items-center p-12'>
+        <video className='w-72' playsInline ref={myVideoRef} autoPlay />
     </div>
   );
 };
-
-export default PeerPage;
+export default PeerPage
